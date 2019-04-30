f21e18ee484179ccdab7803cdf31cd0c3bee6cef
Kausthob Das <kausthob.das@slalom.com>
2019-04-30 16:46:34 -0400

    My new update to check CI pipeline

--- .travis/push.sh
+++ .travis/push.sh
@@ -13,7 +13,7 @@ commit_website_files() {
 
 upload_files() {
   git remote add origin-pages https://${GH_TOKEN}@github.com/kausthob/sfdx-travisci.git > /dev/null 2>&1
-  git push --quiet --set-upstream
+  git push --quiet --set-upstream  origin master
 }
 
 setup_git
