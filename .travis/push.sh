3c8cc2b8d5c9495ef65dc2b8b1bf77cda10cb607
Kausthob Das <kausthob.das@slalom.com>
2019-05-01 12:02:55 -0400

    Tavis CI Automating build script v3

--- .travis/push.sh
+++ .travis/push.sh
@@ -8,11 +8,11 @@ setup_git() {
 commit_website_files() {
   git checkout -b master
   git add .
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER $GH_TOKEN"
 }
 
 upload_files() {
-  git remote add origin-pages https://575e16943643c189cc1aa83b7758616605d23d11@github.com/kausthob/sfdx-travisci.git > /dev/null 2>&1
+  git remote add origin-pages https://$GH_TOKEN@github.com/kausthob/sfdx-travisci.git > /dev/null 2>&1
   git push --quiet --set-upstream  origin master
 }
 
